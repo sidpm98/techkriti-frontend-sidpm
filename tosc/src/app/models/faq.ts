d9e142265e745a03095a67d1427216381d1138f3
Subhdeep Saha <subhdeepsaha@gmail.com>
2017-06-28 19:00:40 +0530

    Changes suggested by parth

--- tosc/src/app/models/faq.ts
+++ tosc/src/app/models/faq.ts
@@ -6,11 +6,11 @@ export interface FAQ {
 export const FAQS: FAQ[] = [
   {
     question: 'What is TOSC?',
-    answer: 'Techkriti Open School Championship is an annual school aptitude examination for classes 9th to 12th, conducted and organized by Techkriti. It consists of 3 Rounds. Round 1 is a written exam, based on which 1000 students will be asked to submit (online) the abstract of the project in Round 2 and for the final Round we will invite 100 selected students at IIT Kanpur to give a presentation about their abstract and win lots of amazing prizes'
+    answer: 'Techkriti Open School Championship is an annual school aptitude examination for classes 9th to 12th, conducted and organized by Techkriti,IIT Kanpur. It consists of 3 Rounds. Round 1 is a written exam, based on which 1000 students will be asked to submit (online) the abstract of the project in Round 2 and for the final Round we will invite 100 selected students at IIT Kanpur to give a presentation about their abstract and win lots of amazing prizes'
   },
   {
     question: 'What is Techkriti?',
-    answer: 'Techkriti is the annual Technical and Entrepreneurial festival of IIT Kanpur to be held from 2nd - 5th March 2017. Techkriti is an annual international technological and entrepreneurial festival organized by the students of Indian Institute of Technology, Kanpur. Etymologically, the word "Techkriti" is composed of two words, "Tech" in English which stands for technology and "Kriti" in Sanskrit stands for creation. Hence, the word "Techkriti" signifies creation or innovation in technology. Techkriti was first held in 1995, and has since grown into one of the largest technical festivals in Asia. The festival is held over four days every March, attracting participants from every corner of the country as well as from abroad, comprising a total footfall of over 25,000 from all over India and abroad.'
+    answer: 'Techkriti is the annual Technical and Entrepreneurial festival of IIT Kanpur to be held from 15th - 18th March 2018. Techkriti is an annual international technological and entrepreneurial festival organized by the students of Indian Institute of Technology, Kanpur. Etymologically, the word "Techkriti" is composed of two words, "Tech" in English which stands for technology and "Kriti" in Sanskrit stands for creation. Hence, the word "Techkriti" signifies creation or innovation in technology. Techkriti was first held in 1995, and has since grown into one of the largest technical festivals in Asia. The festival is held over four days every March, attracting participants from every corner of the country as well as from abroad, comprising a total footfall of over 40,000 from all over India and abroad.'
   },
   {
     question: 'Where will the examination be held?',
@@ -22,7 +22,7 @@ export const FAQS: FAQ[] = [
   },
   {
     question: 'What is the eligibility criteria?',
-    answer: 'You must be studying in standard 9th-12th in the year 2015-16. Classes 9th and 10th comprise the pool A while classes 11th and 12th comprise the pool B. No pre-requisite knowledge is required for solving questions.'
+    answer: 'You must be studying in standard 9th-12th in the year 2016-17. Classes 9th and 10th comprise the pool A while classes 11th and 12th comprise the pool B. No prerequisite knowledge is required for solving questions.'
   },
   {
     question: 'What if two participants equal marks?',
@@ -30,7 +30,8 @@ export const FAQS: FAQ[] = [
   },
   {
     question: 'Even though my school is not tied up with TOSC, can I participate?',
-    answer: 'Yes. Every student pursuing school (class 9th-12th) can enroll for TOSC. Exam centre allotted is also irrespective of School you are from.'},
+    answer: 'Yes. Every student pursuing school (class 9th-12th) can enroll for TOSC. Exam centre allotted is also irrespective of School you are from.'
+  },
   {
     question: 'How to pay the fee?',
     answer: 'A nominal participation fee is charged from each student. Details about the fee / mode of payment etc. are given in the Online Registration Form on the TOSC website.'
@@ -43,4 +44,4 @@ export const FAQS: FAQ[] = [
     question: 'Are droppers eligible for TOSC?',
     answer: 'No, They are not eligible to participate in TOSC.'
   }
-]
+];
