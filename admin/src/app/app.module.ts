36dfcabfc336e00a36dbbce6aafd3a170e1c8e3f
Subhdeep Saha <subhdeepsaha@gmail.com>
2017-07-12 02:59:06 +0530

    Add Angular Material

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app.routing.module';
+import { MaterialModule } from '@angular/material';
+import { NgUploaderModule } from 'ngx-uploader';
 
 // Components
 import { AppComponent } from './app.component';
@@ -12,12 +15,14 @@ import { CaListComponent } from './components/ca-list/ca-list.component';
 import { CaDetailComponent } from './components/ca-detail/ca-detail.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { BackendComponent } from './components/backend/backend.component';
 
 // Services
 import { CAService } from './services/ca.service';
 import { ParamsService } from './services/params.service';
 import { TechHttp } from './services/tech-http.service';
 import { AuthService } from './services/auth.service';
+import { ToscComponent } from './components/tosc/tosc.component';
 
 @NgModule({
   declarations: [
@@ -26,13 +31,18 @@ import { AuthService } from './services/auth.service';
     CaDetailComponent,
     CaListComponent,
     LoginComponent,
-    DashboardComponent
+    BackendComponent,
+    DashboardComponent,
+    ToscComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
+    MaterialModule,
+    NgUploaderModule
   ],
   providers: [
     CAService,
---
+++ admin/src/app/components/tosc/tosc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tosc',
+  templateUrl: './tosc.component.html',
+  styleUrls: ['./tosc.component.css']
+})
+export class ToscComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
